f1c1470d6b314c36da6037ae73c30752f225317f
xyndall <tyrel.ratima@gmail.com>
2022-06-06 15:29:22 +1200

    UnityProject
    I forgot to add to gitkraken first so all my work will be on the one branch but it is fine.

---
+++ Assets/Tyrel/Scripts/PlayerTwo.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTwo : MonoBehaviour
+{
+    public Bullet _bulletPrefab;
+
+
+    public float _thrustSpeed;
+    public float _turnSpeed;
+    public float _reverseSpeed;
+
+    private bool _thrusting;
+    private bool _reverse;
+    private float _turnDir;
+
+    Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _thrusting = Input.GetKey(KeyCode.UpArrow);
+        _reverse = Input.GetKey(KeyCode.DownArrow);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _turnDir = 1.0f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _turnDir = -1f;
+        }
+        else
+        {
+            _turnDir = 0.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Greater))
+        {
+            Shoot();
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (_thrusting)
+        {
+            _rb.AddForce(transform.up * _thrustSpeed);
+        }
+
+        if (_reverse)
+        {
+            _rb.AddForce(transform.up * _reverseSpeed);
+        }
+
+        if (_turnDir != 0)
+        {
+            _rb.AddTorque(_turnDir * _turnSpeed);
+        }
+
+    }
+
+    void Shoot()
+    {
+        Bullet bullet = Instantiate(_bulletPrefab, transform.position, transform.rotation);
+        bullet.Project(transform.up);
+        Debug.Log("Shoot");
+    }
+}
